04511540dddc7749442385a31759bc53fa0716d8
Ayush Chauhan <aac9095@gmail.com>
2017-01-30 22:30:57 +0530

    Step 2 implemented

--- app/src/main/java/aac/scanner/MainActivity.java
+++ app/src/main/java/aac/scanner/MainActivity.java
@@ -1,5 +1,9 @@
 package aac.scanner;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,8 +19,20 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+import static java.lang.Boolean.TRUE;
+import static org.opencv.imgproc.Imgproc.contourArea;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
@@ -39,17 +55,30 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     private Mat rgba,gray,canny;
 
-    final Fabric fabric = new Fabric.Builder(this)
-            .kits(new Crashlytics())
-            .debuggable(true)
-            .build();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(fabric);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        try{
+            final Fabric fabric = new Fabric.Builder(this)
+                    .kits(new Crashlytics())
+                    .debuggable(true)
+                    .build();
+            Fabric.with(fabric);
+        } catch (Exception e){
+            Log.e(TAG,"Exception",e);
+        }
+
+        int hasReadPermission = ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.CAMERA);
+        if(hasReadPermission != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    10);
+        }
 
         javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
         javaCameraView.setVisibility(View.VISIBLE);
@@ -72,10 +101,35 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        List<MatOfPoint> contours = new ArrayList<>();
+        MatOfPoint2f matOfPoint2f_1 ;
+        MatOfPoint2f approxCurves = new MatOfPoint2f();
+        MatOfPoint2f approx = new MatOfPoint2f();
+        MatOfPoint mat = new MatOfPoint();
+        Double epsilon, maxArea = 0.0, area;
         rgba = inputFrame.rgba();
-        Imgproc.GaussianBlur(rgba,gray,new Size(5,5),0);
+        Imgproc.cvtColor(rgba,gray,Imgproc.COLOR_RGBA2GRAY);
+        Imgproc.GaussianBlur(gray,gray,new Size(5,5),0);
         Imgproc.Canny(gray,canny,75,200);
-        return canny;
+        Imgproc.findContours(canny,contours, new Mat(),Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        for(int i=0;i<contours.size();i++){
+            area = contourArea(contours.get(i));
+            matOfPoint2f_1 = new MatOfPoint2f(contours.get(i).toArray());
+            epsilon = Imgproc.arcLength(matOfPoint2f_1,TRUE)*0.02;
+            Imgproc.approxPolyDP(matOfPoint2f_1,approxCurves,epsilon,TRUE);
+
+            if(area > maxArea && approxCurves.total() == 4){
+                maxArea = area;
+                approx = approxCurves;
+            }
+        }
+
+        contours.clear();
+        approx.convertTo(mat,CvType.CV_32S);
+        contours.add(mat);
+        Imgproc.drawContours(rgba,contours,-1, new Scalar(0,255,0),2);
+        return rgba;
     }
 
     @Override
